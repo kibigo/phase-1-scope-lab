72e36aa643f9fbe511164b5c686eb583f6e5e682
Kibigo <jacksonkibigo99@gmail.com>
2023-07-10 17:26:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var customerName = "bob";
+
 const leastFavoriteCustomer = "james";
 
 function upperCaseCustomerName(){
     customerName = customerName.toUpperCase();
 }
+
 function setBestCustomer(){
     bestCustomer = "not bob";
 }
+
 function overwriteBestCustomer(){
     bestCustomer = "maybe bob";
 }
